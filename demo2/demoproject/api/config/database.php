f1f3340749c51931a4a9fbce2aa3fde32d418867
Jessealbonian <157551756+Jessealbonian@users.noreply.github.com>
2025-08-20 07:08:43 +0800

    test

--- demo2/demoproject/api/config/database.php
+++ demo2/demoproject/api/config/database.php
@@ -24,17 +24,29 @@ define("PASSWORD", $pass);
 define("DRIVER", "mysql");
 
 class Connection{
-    private $connectionString = DRIVER . ":host=" . SERVER . ";port=" . $port . ";dbname=" . DATABASE . "; charset=utf8mb4";
+    private $host;
+    private $port;
+    private $db;
+    private $user;
+    private $pass;
     private $options = [
         \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
         \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
         \PDO::ATTR_EMULATE_PREPARES => false
     ];
 
+    public function __construct() {
+        $this->host = SERVER;
+        $this->port = getenv('MYSQLPORT') ?: '3306';
+        $this->db = DATABASE;
+        $this->user = USER;
+        $this->pass = PASSWORD;
+    }
 
     public function connect(){
-        error_log("Attempting database connection with: " . $this->connectionString);
-        return new \PDO($this->connectionString, USER, PASSWORD, $this->options);
+        $connectionString = DRIVER . ":host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db . "; charset=utf8mb4";
+        error_log("Attempting database connection with: " . $connectionString);
+        return new \PDO($connectionString, $this->user, $this->pass, $this->options);
     }
 }
 
