4c277123acdbe7856cc07d1b589d99b4645fe056
Jessealbonian <157551756+Jessealbonian@users.noreply.github.com>
2025-08-20 03:38:28 +0800

    asdfsdf

--- demo2/demoproject/index.php
+++ demo2/demoproject/index.php
@@ -1,1 +1,10 @@
-testing
+<?php
+// ===== CONFIG =====
+$allowed_origins = [
+    "https://athletrack.vercel.app",
+    "https://athletrack-git-main-kohitrees-projects.vercel.app",
+    "https://athletrack-kicwhvrgv-kohitrees-projects.vercel.app",
+    "http://localhost:4200",
+    "https://capstonebackend-9wrj.onrender.com"
+];
+?>
