eda5055a910b90ac2bafc618cd013b1d0ffc3958
Jessealbonian <157551756+Jessealbonian@users.noreply.github.com>
2025-11-06 00:31:38 +0800

    test

--- demo2/demoproject/api/routes.php
+++ demo2/demoproject/api/routes.php
@@ -13,7 +13,8 @@ $allowed_origins = [
     "https://athletrack-git-main-kohitrees-projects.vercel.app",
     "https://athletrack-kicwhvrgv-kohitrees-projects.vercel.app",
     "http://localhost:4200",
-    "https://capstonebackend-9wrj.onrender.com"
+    "https://capstonebackend-9wrj.onrender.com",
+    "https://www.athletrack.site"
 ];
 
 $origin = $_SERVER['HTTP_ORIGIN'] ?? "";
